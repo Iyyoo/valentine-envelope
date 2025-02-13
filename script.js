0c0a53dc8b1a3c6c354f94bd2e9a1bc198422709
Iyyoo <alifofairuz@gmail.com>
2025-02-13 15:34:41 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let passcode = "1234";
+let input = "";
+
+function pressKey(num) {
+    if (input.length < 4) {
+        input += num;
+        updateDisplay();
+    }
+}
+
+function deleteKey() {
+    input = input.slice(0, -1);
+    updateDisplay();
+}
+
+function updateDisplay() {
+    let hearts = document.querySelectorAll(".heart");
+    hearts.forEach((heart, index) => {
+        heart.style.opacity = index < input.length ? "1" : "0.3";
+    });
+}
+
+function checkPasscode() {
+    if (input === passcode) {
+        window.location.href = "valentine.html";
+    } else {
+        document.getElementById("message").innerText = "Passcode salah!";
+        input = "";
+        updateDisplay();
+    }
+}
